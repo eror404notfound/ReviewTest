1ef711c9ce28b15ab05b1673f354758a9e46b69c
Ankit <ankitcool4370@gmail.com>
2022-11-07 09:45:06 +0530

    Demo

---
+++ ReviewTest2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewTest2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Test test = new Test();
+            Console.WriteLine("enter a first  number: ");
+            test.a = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine("enter a second  number: ");
+            test.b = Convert.ToInt32(Console.ReadLine());
+            test.AddTwoNumber(test.a, test.b);
+
+        }
+    }
+}
---
+++ ReviewTest2/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewTest2
+{
+    internal class Test 
+    {
+        public int a;
+        public int b;
+        public void AddTwoNumber(int firstnumber,int secondnumber )
+        {
+            firstnumber = a;
+            secondnumber = b;   
+
+            int x = firstnumber + secondnumber;
+            Console.WriteLine(x);
+
+
+
+        }
+
+    }
+}
